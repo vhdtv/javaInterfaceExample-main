74ea2c957b8e3d388c737cfc00c19ed8e705a614
Victor Dias <vhdtv14@gmail.com>
2024-04-01 21:26:38 -0300

    Exemplo Java Interface

---
+++ src/ConjutoDeNumeros.java
@@ -0,0 +1,7 @@
+public interface ConjutoDeNumeros {
+    public void inserirNumero(int n);
+    public int buscarNumero(int n);
+    public int tamanho();
+    public void ordernar();
+    public int numero(int index);
+}
